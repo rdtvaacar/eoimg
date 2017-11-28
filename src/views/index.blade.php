362a36c659f758d18cd2b6475dc0282a0fee9bc5
acr <acarbey15@gmail.com>
2017-11-28 11:06:59 +0300

    yaz

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -5,16 +5,17 @@
 @section('acr_index')
 
     <div class="container" style="width: 100%;">
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:728px;height:90px"
+             data-ad-client="ca-pub-9863574640622060"
+             data-ad-slot="7704625701"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
         <div class="col-md-7">
             <div class="alert alert-info">Fotoğrafları <strong>"Dosyaları Seç"</strong> butonundan seçin ve yükleme işlemini bitirin ardından <strong>"Arşivle ve İndir" </strong> butonuna basarak bilgisayarrınıza kaydedin.</div>
             <!-- e-okul-resim -->
-            <ins class="adsbygoogle"
-                 style="display:inline-block;width:728px;height:90px"
-                 data-ad-client="ca-pub-9863574640622060"
-                 data-ad-slot="7704625701"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
+
             <a href="/acr/eoimg/arsivle/indir?acr_file_id={{$acr_file_id}}" style="float: right" class="btn btn-warning btn-lg">Arşivle ve İndir</a>
             <div style="margin-left: auto; margin-right: auto;"> {!! AcrFile::form() !!}</div>
         </div>
